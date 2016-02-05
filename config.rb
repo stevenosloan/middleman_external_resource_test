d84779c6142b535c3f2d4381ca163bc05492b82c
Steven Sloan <stevenosloan@gmail.com>
2016-02-05 16:45:33 -0500

    use files.watch per suggestion of @tdreyno

--- config.rb
+++ config.rb
@@ -7,13 +7,20 @@ if Middleman::VERSION >= '4.0'
   import_file File.join( __dir__, 'import_file_dir', 'import.html.erb' ),
               'import.html'
 
+  import_file File.join( __dir__, 'import_file_dir', 'frontmatter.html.erb' ),
+              'frontmatter.html'
+
+  files.watch :source, path: File.join( __dir__, 'import_path_dir' )
+  files.watch :source, path: File.join( __dir__, 'import_file_dir' )
+
 else
   require_relative 'lib/three_four_extension'
   activate :three_four do |c|
     c.files = {
       'import_path_dir/outside_source.html.md'       => 'outside_source.html',
       'import_path_dir/data_outside_source.html.erb' => 'data_outside_source.html',
-      'import_file_dir/import.html.erb'              => 'import.html'
+      'import_file_dir/import.html.erb'              => 'import.html',
+      'import_file_dir/frontmatter.html.erb'         => 'frontmatter.html'
     }
   end
 end
